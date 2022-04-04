9d3dd1afe1d56bc05ffc880d2913576ebad24f3a
DanilDanylchenko <danild@superplay.co>
2022-04-04 18:22:02 +0300

    create helm

--- createbuilder.sh
+++ createbuilder.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/GoogleCloudPlatform/cloud-builders-community.git
  cd cloud-builders-community/helm
- GOOGLE_CLOUD_PROJECT=open-search
+ GOOGLE_CLOUD_PROJECT=open-search-345216
  GCR_HELM=gcr.io/$GOOGLE_CLOUD_PROJECT/helm
  docker build -t $GCR_HELM .
  docker push $GCR_HELM
