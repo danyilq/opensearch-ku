8465f8b43ebce7333d4327a8ef84c6d3c8099fb3
DanilDanylchenko <danild@superplay.co>
2022-04-04 15:32:30 +0300

    few files

---
+++ application.py
@@ -0,0 +1,14 @@
+a = 10
+
+b = 7
+boolean = True, False
+if True:
+    print('true')
+if False:
+    print('false')
+if a % 2 == 0 or b % 2 == 0:
+    print(1)
+else:
+    print(2)
+
+print(a%2 == 0, b%2 == 0)
